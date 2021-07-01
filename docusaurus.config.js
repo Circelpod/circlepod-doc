56ef515277475f038f5728454e6ea7eccebf8f5e
yasuoyuhao <yasuoyuhao@klearthink.com>
2021-07-01 23:03:04 +0800

    fix(i18n): 加入中文譯文

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,11 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
+          href: 'https://www.circlepod.app',
+          label: 'Home',
+          position: 'right',
+        },
+        {
           href: 'https://github.com/Circelpod',
           label: 'GitHub',
           position: 'right',
